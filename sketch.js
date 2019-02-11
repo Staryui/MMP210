41c55f364907c5947c3e141fc97228a765666ecb
Staryui <42847225+Staryui@users.noreply.github.com>
2019-02-10 23:23:25 -0500

    hw update

---
+++ sketch.js
@@ -0,0 +1,56 @@
+let snowflakes = []; // array to hold snowflake objects
+
+function setup() {
+  var cnv = createCanvas(400, 600);
+    cnv.parent=('content');
+  fill(100);
+  noStroke();
+}
+
+function draw() {
+  background('orange');
+  let t = frameCount / 20; // update time
+
+  // create a random number of snowflakes each frame
+  for (var i = 0; i < random(1); i++) {
+    snowflakes.push(new snowflake()); // append snowflake object
+  }
+
+  // loop through snowflakes with a for..of loop
+  for (let flake of snowflakes) {
+    flake.update(t); // update snowflake position
+    flake.display(); // draw snowflake
+  }
+}
+// snowflake class
+function snowflake() {
+  // initialize coordinates
+  this.posX = 0;
+  this.posY = random(-50, 0);
+  this.initialangle = random(0, 2 * PI);
+  this.size = random(2, 5);
+
+  // radius of snowflake spiral
+  // chosen so the snowflakes are uniformly spread out in area
+  this.radius = sqrt(random(pow(width / 2, 2)));
+
+  this.update = function(time) {
+    // x position follows a circle
+    let w = 0.6; // angular speed
+    let angle = w * time + this.initialangle;
+    this.posX = width / 2 + this.radius * sin(angle);
+
+    // different size snowflakes fall at slightly different y speeds
+    this.posY += pow(this.size, 0.9);
+
+    // delete snowflake if past end of screen
+    if (this.posY > height) {
+      let index = snowflakes.indexOf(this);
+      snowflakes.splice(index, 1);
+    }
+  };
+
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size);
+  };
+}
---
+++ sketchp1p.js
@@ -0,0 +1,27 @@
+
+function setup() {
+  var cnv = createCanvas(500, 400);}
+  
+    
+    function draw() {
+  background(150);
+ 
+        
+noStroke();//head weight
+fill('pink');
+        circle (254, 205, 100)  //head
+ stroke(1);
+        strokeWeight(3);
+       fill(250);
+
+    ellipse(205, 200,40,); //eyeleft
+    ellipse(300, 200, 40,); //eyeright
+
+       
+        strokeWeight(1);//nose weight
+        
+        triangle( 250, 244, 240, 222, 263, 223)  
+
+
+    }
+
